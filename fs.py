c9f01c23db5c37160e1716e2ee13a10fc441afbb
chrisking94 <1050243371@qq.com>
2023-06-25 17:15:25 +0800

    add 'fs.py'

--- fs.py
+++ fs.py
@@ -3,6 +3,9 @@ from webdav3.client import Client
 
 
 class FilesystemClient(ABC):
+    def __init__(self, root: str):
+        self._root = root
+
     @abstractmethod
     def upload(self, local_fp: str, remote_fp: str):
         pass
@@ -15,9 +18,13 @@ class FilesystemClient(ABC):
     def makedirs(self, remote_path: str):
         pass
 
+    def _abspath(self, remote_path: str):
+        return self._root + remote_path
+
 
 class WebDAVFolder(FilesystemClient):
-    def __init__(self, host, username, password):
+    def __init__(self, host, username, password, root="/"):
+        super().__init__(root)
         options = {
             'webdav_hostname': host,
             'webdav_login': username,
@@ -28,10 +35,10 @@ class WebDAVFolder(FilesystemClient):
         self.client = client
 
     def upload(self, local_fp: str, remote_fp: str):
-        self.client.upload(remote_fp, local_fp)
+        self.client.upload(self._abspath(remote_fp), local_fp)
 
     def download(self, remote_fp: str, local_fp: str):
-        self.client.download(remote_fp, local_fp)
+        self.client.download(self._abspath(remote_fp), local_fp)
 
     def makedirs(self, remote_path: str):
-        self.client.mkdir(remote_path)
+        self.client.mkdir(self._abspath(remote_path))
